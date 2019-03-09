c1b929e3964a39313612af681e827c81e9bce328
zwjphp <834320733@qq.com@example.com>
2019-03-09 22:43:54 +0800

    测试二维码

--- wc_1.php
+++ wc_1.php
@@ -1,9 +1,9 @@
 <?php
 
 require './WeChat.php';
-define('APPID','wx83fe6ac00511b3b0');
-define('APPSECRET','92c96d806d530d718fc29c1684c198b6');
-$wechat = new WeChat(APPID, APPSECRET);
+require './app.conf.php';
+
+$wechat = new WeChat(APPID, APPSECRET, TOKEN);
 
 //获取 access_token
 //$access_token = $wechat->getAccessToken();
@@ -16,5 +16,5 @@ $wechat = new WeChat(APPID, APPSECRET);
 //$wechat->getQRCodeTicket(123,1);
 //$wechat->getQRCodeTicket('123',1);
 
-//$wechat->getQRCode('123','./1234.jpg',$wechat::QRCODE_TYPE_TEMP);
 $wechat->getQRCode('123','',$wechat::QRCODE_TYPE_TEMP);
+//$wechat->getQRCode('123','',$wechat::QRCODE_TYPE_TEMP);
