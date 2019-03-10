46bff731c1c924cf2ab2d1857bd82108178cf4ad
zwjphp <834320733@qq.com@example.com>
2019-03-10 21:26:59 +0800

    添加菜单

--- wc_1.php
+++ wc_1.php
@@ -6,9 +6,9 @@ require './app.conf.php';
 $wechat = new WeChat(APPID, APPSECRET, TOKEN);
 
 //获取 access_token
-//$access_token = $wechat->getAccessToken();
+$access_token = $wechat->getAccessToken();
 //
-//echo $access_token;
+echo $access_token;
 
 //获取二维码
 //$result = $wechat->getQRCodeTicket(10000001);
@@ -16,5 +16,5 @@ $wechat = new WeChat(APPID, APPSECRET, TOKEN);
 //$wechat->getQRCodeTicket(123,1);
 //$wechat->getQRCodeTicket('123',1);
 
-$wechat->getQRCode('123','',$wechat::QRCODE_TYPE_TEMP);
+//$wechat->getQRCode('123','',$wechat::QRCODE_TYPE_TEMP);
 //$wechat->getQRCode('123','',$wechat::QRCODE_TYPE_TEMP);
